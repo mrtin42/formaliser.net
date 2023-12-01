0798c63f9cca3183ebae5707a7a89ace7bcbc0ca
martin <martin@tub3.uk>
2023-12-01 22:05:44 +0000

    remove tail

--- emails/incoming.tsx
+++ emails/incoming.tsx
@@ -6,36 +6,35 @@ export default function Email({ name, email, subject, message }: { name: string,
         <>
             <Html lang="en">
                 <MailerHead />
-                <Tailwind config={{theme: {extend: {fontFamily: {sans: ['Inter', 'sans-serif']}}}}}>
-                    <div className="bg-gray-100">
-                        <div className="container mx-auto px-4 font-sans w-10/12">
-                            <header className="py-3 flex justify-center items-center">
-                                <div className="flex items-center">
-                                    <img className="h-12" src="https://formaliser.net/formaliser.svg" alt="FORMALISER.NET" />
-                                </div>
-                            </header>
-                            <div className="py-4 flex justify-center items-center flex-col">
-                                <h1 className="text-2xl font-semibold text-gray-900 py-0 my-0">Webform Submission</h1>
-                                <p className="text-gray-700 padding-1 py-0 my-1">You have received a webform submission.</p>
+                {/* getting rid of Tailwind for now cos its slowing rendering down and consequently causing timeouts */}
+                <div className="bg-gray-100">
+                    <div className="mx-auto px-4 font-sans w-10/12">
+                        <header className="py-3 flex justify-center items-center">
+                            <div className="flex items-center">
+                                <img className="h-12" src="https://formaliser.net/formaliser.svg" alt="FORMALISER.NET" />
                             </div>
-                            <hr className="border-gray-300  w-10/12" />
-                            <div className="py-4 flex justify-center items-center flex-col">
-                                <h2 className="text-xl font-semibold text-gray-900 py-0 my-0">From: {name} ({email})</h2>
-                                <h4 className="text-gray-700 padding-1 py-0 my-1">Subject: {subject}</h4>
-                                <p className="text-gray-700 text-center py-0 my-1 px-2">* * *</p>
-                                <p className="text-gray-700 padding-1 py-0 my-1">{message}</p>
-                            </div>
-                            <hr className="border-gray-300  w-10/12" />
-                            <div className="py-4 flex justify-center items-center flex-col">
-                                <p className="text-gray-700 padding-1 py-0 my-1">Hit reply to respond to this message. The sender address will be automatically filled in by your email client.</p>
-                            </div>
-                            <footer className="py-3 flex flex-col justify-center items-center">
-                                <p className="text-gray-700 padding-1 py-0 my-1 text-xs italic">HTML webforms powered by <a className="text-blue-500 no-underline" href="https://formaliser.net">FORMALISER.NET</a></p>
-                                <p className="text-gray-700 padding-1 py-0 my-1 text-xs italic">To unsubscribe from these emails, remove our URL from your webform action attribute.</p>
-                            </footer>
+                        </header>
+                        <div className="py-4 flex justify-center items-center flex-col">
+                            <h1 className="text-2xl font-semibold text-gray-900 py-0 my-0">Webform Submission</h1>
+                            <p className="text-gray-700 padding-1 py-0 my-1">You have received a webform submission.</p>
                         </div>
+                        <hr className="border-gray-300  w-10/12" />
+                        <div className="py-4 flex justify-center items-center flex-col">
+                            <h2 className="text-xl font-semibold text-gray-900 py-0 my-0">From: {name} ({email})</h2>
+                            <h4 className="text-gray-700 padding-1 py-0 my-1">Subject: {subject}</h4>
+                            <p className="text-gray-700 text-center py-0 my-1 px-2">* * *</p>
+                            <p className="text-gray-700 padding-1 py-0 my-1">{message}</p>
+                        </div>
+                        <hr className="border-gray-300  w-10/12" />
+                        <div className="py-4 flex justify-center items-center flex-col">
+                            <p className="text-gray-700 padding-1 py-0 my-1">Hit reply to respond to this message. The sender address will be automatically filled in by your email client.</p>
+                        </div>
+                        <footer className="py-3 flex flex-col justify-center items-center">
+                            <p className="text-gray-700 padding-1 py-0 my-1 text-xs italic">HTML webforms powered by <a className="text-blue-500 no-underline" href="https://formaliser.net">FORMALISER.NET</a></p>
+                            <p className="text-gray-700 padding-1 py-0 my-1 text-xs italic">To unsubscribe from these emails, remove our URL from your webform action attribute.</p>
+                        </footer>
                     </div>
-                </Tailwind>
+                </div>
             </Html>
         </>
     )

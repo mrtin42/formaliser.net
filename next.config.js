fc2317fd2c994c24dee7e19caf17130a9581634c
martin <martin@tub3.uk>
2023-12-01 20:23:54 +0000

    could someone please kill me al

--- next.config.js
+++ next.config.js
@@ -1,12 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    webpack: (config) => {
-        config.resolve.fallback = {
-            ...config.resolve.fallback,
-            stream: require.resolve('stream-browserify'),
-            crypto: require.resolve('crypto-browserify'),
-        }
-    }
-}
+const nextConfig = {}
 
 module.exports = nextConfig

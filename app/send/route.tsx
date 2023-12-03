eb83c96dc800cb0d87b2b762160cd43979b6596d
martin <martin@tub3.uk>
2023-12-03 11:32:16 +0000

    dotted console.logs across the mailer rout

--- app/send/route.tsx
+++ app/send/route.tsx
@@ -1,79 +1,11 @@
-// import Email from "@/emails/incoming";
-// import { render } from '@react-email/render';
-// import nodemailer from 'nodemailer';
-// import axios from 'axios';
-// export const dynamic = 'force-dynamic';
-
-// // export const runtime = "edge";
-// // you know what, screw that ^ - it's just fucking nodemailer over. lets see if plain text rendering is the issue
-
-// export async function POST(req: Request) {
-//     const { name, email, subject, message } = await req.json();
-//     const { searchParams } = new URL(req.url);
-//     const sendto = searchParams.get('to');
-
-//     if (sendto === null) {
-//         // send norecipient.html
-//         const html: string = await axios.get('https://formaliser.net/emptyfields.html');
-//         return new Response(html, { status: 500 });
-//     }
-
-//     if (!name || !email || !subject || !message) {
-//         // send missingfields.html
-//         const html: string = await axios.get('https://formaliser.net/emptyfields.html');
-//         return new Response(html, { status: 500 });
-//     }
-
-//     if (!email.includes('@') || !email.includes('.')) {
-//         // send invalidemail.html
-//         const html: string = await axios.get('https://beta.formaliser.net/invalidemail.html');
-//         return new Response(html, { status: 500 });
-//     }
-
-//     const html = render(<Email name={name} email={email} subject={subject} message={message} />);
-//     const plain = `NEW FORMALISER.NET MESSAGE\n\nName: ${name}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}\nThis message is better viewed on a client that supports HTML emails.`
-
-//     const transporter = nodemailer.createTransport({
-//         host: process.env.SMTP_HOST,
-//         port: 587,
-//         secure: false,
-//         auth: {
-//             user: process.env.SMTP_USER,
-//             pass: process.env.SMTP_PASS,
-//         }
-//     });
-//     const emailOptions = {
-//         from: 'incoming@formaliser.net',
-//         to: sendto,
-//         replyTo: email,
-//         subject: `New message from ${name}`,
-//         html: html,
-//         text: plain,
-//     }
-
-//     await transporter.sendMail(emailOptions, async (err, info) => {
-//         if (err) {
-//             console.log(err);
-//             const html: string = await axios.get('https://beta.formaliser.net/servererror.html');
-//             return new Response(html, { status: 500 });
-//         }
-//         console.log(err);
-//         console.log(info);
-//         const html: string = await axios.get('https://beta.formaliser.net/success.html');
-//         return new Response(html, { status: 200 });
-//     });
-// }
-
-// lets try this again
-
 import Email from "@/emails/incoming";
 import { render } from '@react-email/render';
 import nodemailer from 'nodemailer';
 import axios from 'axios';
-import util from 'util';
 import { NextRequest } from 'next/server';
 
-// prerender the email with ${name} ${email} ${subject} ${message} as placeholders
+// ---------------------------------------------------------------------------------------------
+
 const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: 587,
@@ -95,32 +27,44 @@ export async function POST(req: NextRequest) {
         const searchParams = req.nextUrl.searchParams;
         const sendto = searchParams.get('to');
 
-        if (sendto === null || !name || !email || !subject || !message) {
-            const html = await axiosGet('https://formaliser.net/emptyfields.html');
-            return new Response(html, { status: 500 });
+        if (!sendto) {
+            console.log('Terminating email render: no recipient specified.')
+            const html = await axiosGet('https://beta.formaliser.net/norecipient.html');
+            return new Response(html, { status: 400 });
+        }
+
+        if (!name || !email || !subject || !message) {
+            console.log('Terminating email render: empty fields.')
+            const html = await axiosGet('https://beta.formaliser.net/emptyfields.html');
+            return new Response(html, { status: 400 });
         }
 
         if (!email.includes('@') || !email.includes('.')) {
+            console.log('Terminating email render: invalid sender email.')
             const html = await axiosGet('https://beta.formaliser.net/invalidemail.html');
-            return new Response(html, { status: 500 });
+            return new Response(html, { status: 400 });
         }
 
         const html = render(<Email name={name} email={email} subject={subject} message={message} />);
+        console.log('Email render successful.')
         const plain = `NEW FORMALISER.NET MESSAGE\n\nName: ${name}\nEmail: ${email}\nSubject: ${subject}\nMessage: ${message}\nThis message is better viewed on a client that supports HTML emails.`
+        console.log('Email plain text render successful.')
 
         const emailOptions = {
-            from: 'incoming@formaliser.net',
+            from: 'FORMALISER.NET Form <incoming@formaliser.net>',
             to: sendto,
-            replyTo: email,
+            replyTo: `${name} <${email}>`, // this is the only way to get the name to show up in the reply-to field
             subject: `New message from ${name}`,
             html: html,
             text: plain,
         };
 
         await transporter.sendMail(emailOptions);
+        console.log('Email render and delivery successful.')
         const successHtml = await axiosGet('https://beta.formaliser.net/success.html');
         return new Response(successHtml, { status: 200 });
     } catch (error) {
+        console.log('Terminating email render: server error.')
         console.error(error);
         const errorHtml = await axiosGet('https://beta.formaliser.net/servererror.html');
         return new Response(errorHtml, { status: 500 });

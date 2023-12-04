72782f3c62c61109ba6a6b976e35664c7aea9876
martin <martin@tub3.uk>
2023-12-04 18:10:39 +0000

    footer

--- app/page.tsx
+++ app/page.tsx
@@ -185,17 +185,17 @@ export default function Home() {
                     </div>
                 </main>
                 <footer className={[styles.footer, 'flex flex-col items-center justify-center w-full h-3/10 bg-slate-900'].join(' ')}>
-                    <div className='flex flex-col items-center justify-center w-full p-10 notgrid'>
-                        <Image src="/formaliser.svg" width={500} height={100} alt="FORMALISER.NET" className='shadow-lg shadow-slate-300'/>
-                        <p className="text-lg text-center text-white-800">
+                    <div className='flex flex-col items-center justify-center w-full p-1 pt-5 notgrid'>
+                        <Image src="/formaliser.svg" width={500} height={100} alt="FORMALISER.NET" className='shadow-lg bg-slate-400 rounded-2xl shadow-slate-300'/>
+                        <p className="text-lg text-center text-white-800 mt-2">
                             © 2023 FORMALISER.NET
                         </p>
                     </div>
-                    <div className='flex flex-col items-center justify-center w-full p-10 notgrid'>
+                    <div className='flex flex-col items-center justify-center w-full mt-1 p-1 pb-5 notgrid'>
                         <p className={["text-lg text-center text-white-800", inter.className].join(' ')}>
                             Made with <span className="text-red-500">❤</span> by <Link className='underline hover:no-underline' href="https://marrtin.com">MAЯTÍN</Link>.
                         </p>
-                    </div>
+                    </div>  
                 </footer>
             </div>
         </>

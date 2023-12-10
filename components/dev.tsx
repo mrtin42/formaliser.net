d7627baa7352fae954f204c0c81c7b0740df2095
martin <martin@tub3.uk>
2023-12-10 22:36:26 +0000

    maybe this works

--- components/dev.tsx
+++ components/dev.tsx
@@ -7,7 +7,7 @@ export default function DevDisclaimer() {
     return (
         <>
             {hostname === 'beta.formaliser.net' ? (
-                <div className='fixed z-50 flex flex-col items-center justify-center w-full h-1/10 bg-slate-900'>
+                <div className='sticky top-0 z-50 flex flex-col items-center justify-center w-full h-1/10 bg-slate-900'>
                     <p className='text-lg text-center text-white-800'>
                         You are currently using the beta version of FORMALISER.NET. Please be aware that this version is not yet stable and may contain bugs. If you find any bugs, please report them to <a className='underline hover:no-underline' href="mailto:info@formaliser.net">info@formaliser.net</a>.
                     </p>

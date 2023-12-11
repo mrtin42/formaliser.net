25655c44a15527cb8085b65234b13c8149a8c049
martin <martin@tub3.uk>
2023-12-11 21:38:57 +0000

    fix metadata not appearing

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -2,7 +2,6 @@ import Head from 'next/head';
 import Link from 'next/link';
 import Image from 'next/image';
 import Script from 'next/script';
-import Metadata from '@/components/meta';
 import DevDisclaimer from '@/components/dev';
 import Navbar from '@/components/nav';
 import SendButton from '@/components/sendbtn';
@@ -25,11 +24,47 @@ export default function Contact() {
     return (
         <>
             <Head>
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta charSet='utf-8' />
                 <title>FORMALISER.NET | Contact</title>
                 <meta name="description" content="Contact FORMALISER.NET." />
                 <link rel="icon" href="/favicon.ico" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" />
-                <Metadata />
+                <meta name="description" content="Free and open source webform delivery" />
+                <meta name="keywords" content="webform, delivery, free, open source, form, forms, form delivery, form delivery service, form delivery services, form delivery service free, backend, backend as a service, baas, backend as a service free, backend as a service open source, backend as a service free open source, free backend as a service, free backend as a service open source, free backend as a service free, free backend as a service free open source, free backend as a service open source free, formaliser, formaliser.net, formaliser free, formaliser open source, formaliser free open source, formaliser free open source free, formaliser free free, formaliser free free open source, formaliser free open source free, formaliser open source free, formaliser open source free free, formaliser open source free open source, formaliser free free free, formaliser free free open source free, formaliser free open source free free, formaliser free open source free open source" />
+                <meta name="author" content="WWW.MARRTIN.COM" />
+                <meta name="robots" content="index, follow" />
+                <meta name="googlebot" content="index, follow" />
+                <meta name="google" content="notranslate" />
+                <meta name="format-detection" content="telephone=no" />
+                <meta name="theme-color" content="#3c52bf" />
+                <meta name="msapplication-navbutton-color" content="#3c52bf" />
+                <meta name="apple-mobile-web-app-status-bar-style" content="#3c52bf" />
+                <meta name="full-screen" content="yes" />
+                <meta name="browsermode" content="application" />
+                {/* time for open graph */}
+                <meta property="og:title" content="FORMALISER.NET" />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://formaliser.net" />
+                <meta property="og:image" content="https://formaliser.net/og.png" />
+                <meta property="og:image:type" content="image/png" />
+                <meta property="og:image:width" content="1200" />
+                <meta property="og:image:height" content="630" />
+                <meta property="og:image:alt" content="FORMALISER.NET" />
+                <meta property="og:description" content="Free and open source webform delivery" />
+                <meta property="og:site_name" content="FORMALISER.NET" />
+                {/* time for twitter */}
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:site" content="@t_ub3" />
+                <meta name="twitter:creator" content="@t_ub3" />
+                <meta name="twitter:title" content="FORMALISER.NET" />
+                <meta name="twitter:description" content="Free and open source webform delivery" />
+                <meta name="twitter:image" content="https://formaliser.net/og.png" />
+                <meta name="twitter:image:alt" content="FORMALISER.NET" />
+
+                <link rel="canonical" href="https://formaliser.net" />
+                <link rel="icon" href="/favicon.ico" />
+
             </Head>
             <div className={[styles.body,'text-slate-100'].join(' ')}>
                 <DevDisclaimer />   
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Link from 'next/link'
 import Image from 'next/image'
-import Metadata from '@/components/meta'
 import DevDisclaimer from '@/components/dev'
 import Navbar from '@/components/nav'
 import Tooltip from '@/components/tooltip'
@@ -35,7 +34,41 @@ export default function Home() {
                 <link style={{}} rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" />
                 <link style={{}} rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter&display=swap" />
                 <link style={{}} rel="stylesheet" href="/failwind.css" />
-                <Metadata />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="description" content="Free and open source webform delivery" />
+                <meta name="keywords" content="webform, delivery, free, open source, form, forms, form delivery, form delivery service, form delivery services, form delivery service free, backend, backend as a service, baas, backend as a service free, backend as a service open source, backend as a service free open source, free backend as a service, free backend as a service open source, free backend as a service free, free backend as a service free open source, free backend as a service open source free, formaliser, formaliser.net, formaliser free, formaliser open source, formaliser free open source, formaliser free open source free, formaliser free free, formaliser free free open source, formaliser free open source free, formaliser open source free, formaliser open source free free, formaliser open source free open source, formaliser free free free, formaliser free free open source free, formaliser free open source free free, formaliser free open source free open source" />
+                <meta name="author" content="WWW.MARRTIN.COM" />
+                <meta name="robots" content="index, follow" />
+                <meta name="googlebot" content="index, follow" />
+                <meta name="google" content="notranslate" />
+                <meta name="format-detection" content="telephone=no" />
+                <meta name="theme-color" content="#3c52bf" />
+                <meta name="msapplication-navbutton-color" content="#3c52bf" />
+                <meta name="apple-mobile-web-app-status-bar-style" content="#3c52bf" />
+                <meta name="full-screen" content="yes" />
+                <meta name="browsermode" content="application" />
+                {/* time for open graph */}
+                <meta property="og:title" content="FORMALISER.NET" />
+                <meta property="og:type" content="website" />
+                <meta property="og:url" content="https://formaliser.net" />
+                <meta property="og:image" content="https://formaliser.net/og.png" />
+                <meta property="og:image:type" content="image/png" />
+                <meta property="og:image:width" content="1200" />
+                <meta property="og:image:height" content="630" />
+                <meta property="og:image:alt" content="FORMALISER.NET" />
+                <meta property="og:description" content="Free and open source webform delivery" />
+                <meta property="og:site_name" content="FORMALISER.NET" />
+                {/* time for twitter */}
+                <meta name="twitter:card" content="summary_large_image" />
+                <meta name="twitter:site" content="@t_ub3" />
+                <meta name="twitter:creator" content="@t_ub3" />
+                <meta name="twitter:title" content="FORMALISER.NET" />
+                <meta name="twitter:description" content="Free and open source webform delivery" />
+                <meta name="twitter:image" content="https://formaliser.net/og.png" />
+                <meta name="twitter:image:alt" content="FORMALISER.NET" />
+
+                <link rel="canonical" href="https://formaliser.net" />
+                <link rel="icon" href="/favicon.ico" />
             </Head>
             <div className={[styles.body, 'text-slate-100'].join(' ')}>
                 <DevDisclaimer />
